02a0c62cc5ce5b25c69d27a9e5d49453aa6bc824
rgtachyni <regitha1606@gmail.com>
2024-09-25 22:27:24 +0700

    ubah form

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -12,62 +12,68 @@
 </head>
 
 <body class="bg-bg-pink min-h-screen">
-   <h1 class="text-pink text-5xl font-bold text-center mt-2">GuestBookkkk</h1>www
+   <h1 class="text-pink text-5xl font-bold text-center mt-2 mb-10">GuestBookkkk</h1>
 
-    @if($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+
+@if($errors->any())
+<div class="flex justify-center p-4 mb-4 text-sm text-red-800 rounded-lg  dark:bg-gray-800 dark:text-red-400 " role="alert" >
+    <svg class="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+    </svg>
+    <span class="sr-only">Danger</span>
+    <div>
+      <span class="font-medium">Please fill in</span>
+        <ul class="mt-1.5 list-disc list-inside">
+         @foreach($errors->all() as $error)
+         <li>{{$error}}</li>
+         @endforeach
+      </ul>
     </div>
+  </div>
 @endif
 
-
-<form action="{{route('guest.store')}}" method="POST"  class=" bg-white mx-36 mt-5 rounded-xl py-5 ring-1  ring-black">
-        @csrf
-    <div class="grid grid-cols-2 place-items-center gap-5 ">
-        <div class="" >
-        <label for="nama">Nama </label> <br>
-        <input type="text" id="name" name="name" required class="ring-1 ring-black">
-        </div>
-        <div>
-        <label for="address">Address </label><br>
-        <input type="text" id="address" name="address" required  class="ring-1 ring-black">
+<form action="{{route('guest.store')}}" method="POST"  class="max-w-md mx-auto bg-white rounded-xl px-10 py-5">
+    @csrf
+    <div class="relative z-0 w-full mb-5 group">
+        <input type="text" name="name" id="name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required  value="{{old('name')}}"/>
+        <label for="name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
     </div>
-    <div>
-        <label for="whatshapp">Whatsapp Number </label><br>
-        <input type="number" id="whatshapp" name="whatshapp" required  class="ring-1 ring-black">
+    <div class="relative z-0 w-full mb-5 group">
+        <input type="text" name="address" id="address" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value="{{old('address')}}"/>
+        <label for="address" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Address</label>
     </div>
-    <div>
-        <label for="email">Email </label><br>
-        <input type="email" id="email" name="email" required  class="ring-1 ring-black">
+    <div class="relative z-0 w-full mb-5 group">
+        <input type="number" name="whatshapp" id="whatshapp" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value="{{old('whatshapp')}}" />
+        <label for="whatshapp" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Whatsapp Number</label>
     </div>
-    <div>
-        <label for="organization">Organization/ Agency </label><br>
-        <textarea type="text" id="organization" name="organization" required  class="ring-1 ring-black px-2"></textarea>
+    <div class="relative z-0 w-full mb-5 group">
+        <input type="email" name="email" id="email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value="{{old('email')}}"/>
+        <label for="email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
     </div>
-    <div>
-        <label for="purpose">Purpose of visit </label><br>
-        <textarea type="text" id="purpose" name="purpose" required  class="ring-1 ring-black  px-2"></textarea>
-    </div>      
-</div>
-<div class="grid place-items-center ">
-    <label for="ttd">TTD</label><br>
-    <canvas id="ttd" name="ttd" class="signature-pad" width=400 height=200></canvas>
-<input type="hidden" name="ttd" id="signature">
+    <div class="relative z-0 w-full mb-5 group">
+        <input type="text" name="organization" id="organization" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value="{{old('oranization')}}" />
+        <label for="organization" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Organization
+    </div>
+    <div class="relative z-0 w-full mb-5 group">
+        <input type="text" name="purpose" id="purpose" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value="{{old('purpose')}}"/>
+        <label for="purpose" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Purpose of visit
+    </div>
+    <div class="grid place-items-center ">
+        <label for="ttd">Signature</label><br>
+        <canvas id="ttd" name="ttd" class="signature-pad" width=400 height=100></canvas>
+        <input type="hidden" name="ttd" id="signature" value="{{old('ttd')}}">
+    
+    </div>
+    <div class="flex gap-5 justify-center mb-20">
+   <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" id="save">Save</button>
+   <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" id="clear">Clear</button>
+   <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onclick="window.location.href='{{ route('index')}}'">Cancel</button>
+    </div>
+   
+  
 
-</div>
-<div class="flex justify-center gap-5 py-10">
-<button type="submit" class="bg-bg-pink  rounded-md py-2 px-10 ring-1 ring-black hover:bg-slate-400" id="save">Save</button>
-<button type="submit" class="bg-bg-pink  rounded-md py-2 px-10 ring-1 ring-black hover:bg-slate-400" id="clear">clear</button>
-<button type="submit" class="bg-bg-pink  rounded-md py-2 px-10 ring-1 ring-black hover:bg-slate-400" onclick="window.location.href='{{ route('index')}}'">Cancel</button>
-
-
-
-</div>
 </form>
+  
 
 
 <script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
@@ -88,9 +94,10 @@
                 var signature = signaturePad.toDataURL();
                 document.getElementById('signature').value = signature;
                 document.querySelector('form').submit();  // Submit the form
-            } else {
-                alert("data cannot be empty");
             }
+            //  else {
+            //     alert("data cannot be empty");
+            // }
         });
     });
 </script>
